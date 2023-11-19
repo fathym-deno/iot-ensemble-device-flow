df61a4de416d610f1d29a8131911c63626a17c4f
mcgear <michael.gearhardt@gmail.com>
2023-11-19 13:04:05 -0700

    new function

--- worker.ts
+++ worker.ts
@@ -1,12 +1,14 @@
 import { AzureFunctionsWorker } from './deps.ts';
 
 import hello_world from './functions/hello_world.ts';
+import hello_world2 from './functions/hello_world2.ts';
 import deviceTelemetry from './functions/deviceTelemetry.ts';
 import deviceTelemetryListener from './functions/deviceTelemetryListener.ts';
 import deviceTelemetryNegotiate from './functions/deviceTelemetryNegotiate.ts';
 
 const worker = new AzureFunctionsWorker([
   hello_world,
+  hello_world2,
   deviceTelemetry,
   deviceTelemetryListener,
   deviceTelemetryNegotiate,
